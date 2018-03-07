ec776732bd36bb072ca4002b96e2387aa3b521c5
Aleš Kocur <aleskocur@icloud.com>
2018-03-07 20:48:54 +0000

    Add header support

--- Example/Markdown/test.md
+++ Example/Markdown/test.md
@@ -1,5 +1,9 @@
 #  Nice big title
+## Smaller title
 Content *that* tests **every** possibility. Content _that_ tests __every__ possibility.
 
+### Even smaller title
 Like a piece of `inline code`.
 
+#### This is almost not a title
+But it's still supported…
--- Markdown/Classes/Markdown.swift
+++ Markdown/Classes/Markdown.swift
@@ -14,6 +14,7 @@ public class Markdown {
         parser.add(markExpression: strongExpression)
         parser.add(markExpression: emphasisExpression)
         parser.add(markExpression: monospaceExpression)
+        parser.add(markExpression: headerExpression)
     }
 
     // MARK: - Styling
@@ -26,6 +27,19 @@ public class Markdown {
         .foregroundColor: UIColor(white: 0.1, alpha: 1.0),
         .backgroundColor: UIColor(white: 0.95, alpha: 1.0)
     ]
+
+
+    var headerAttributes: [[NSAttributedStringKey: Any]] = [
+        [.font: UIFont.systemFont(ofSize: 28.0, weight: .bold)],
+        [.font: UIFont.systemFont(ofSize: 24.0, weight: .bold)],
+        [.font: UIFont.systemFont(ofSize: 20.0, weight: .semibold)],
+        [.font: UIFont.systemFont(ofSize: 17.0, weight: .semibold)],
+        [.font: UIFont.systemFont(ofSize: 16.0, weight: .semibold)],
+        [.font: UIFont.systemFont(ofSize: 15.0, weight: .medium)]
+    ]
+
+    typealias EditAction = (inout NSMutableAttributedString, NSRange) -> Void
+    typealias LevelEditAction = (inout NSMutableAttributedString, NSRange, Int) -> Void
 
     // MARK: - Strong
 
@@ -63,6 +77,25 @@ public class Markdown {
         }
     }
 
+    // MARK: - Headers
+
+    private static let headerPattern = "^(#{1,%@})\\s+(.+)$"
+
+    private var headerExpression: MarkExpression {
+        return leadingExpression(pattern: Markdown.headerPattern, maxLevel: 6, formatAction: { (mutableContent, range, level) in
+
+            let attributeLevel = level - 1
+
+            if attributeLevel < self.headerAttributes.count {
+                mutableContent.addAttributes(self.headerAttributes[attributeLevel], range: range)
+            }
+
+        }, markEditAction: { (mutableContent, range, _) in
+            // Just get rid of the ####
+            mutableContent.deleteCharacters(in: range)
+        })
+    }
+
     // MARK: - Helpers
 
     /**
@@ -80,6 +113,32 @@ public class Markdown {
             mutableContent.deleteCharacters(in: match.range(at: 3))
             action(match.range(at: 2), &mutableContent)
             mutableContent.deleteCharacters(in: match.range(at: 1))
+        }
+    }
+
+    /**
+     Finds a leading expressions (ie `# Text` or `* Text`).
+
+     - parameter pattern: Regex expression with that have to contain 2 matchings (symbol)(content).
+     - parameter maxLevel: Max level of repetition (ie #### is level 4)
+     - parameter formatAction: Closure for defining how to format the content in given range (ie set font)
+     - parameter markEditAction: What to do with the mark expression (ie delete it)
+
+     - returns: New expression
+     */
+    private func leadingExpression(pattern: String, maxLevel: Int, formatAction: @escaping LevelEditAction, markEditAction: @escaping LevelEditAction) -> MarkExpression {
+        let regexPattern = String(format: pattern, "\(maxLevel)")
+        let expression = try! NSRegularExpression(pattern: regexPattern, options: [.anchorsMatchLines])
+
+        return MarkExpression(expression: expression) { (match, mutableContent) in
+            let match1Range = match.range(at: 1)
+            let match2Range = match.range(at: 2)
+            let level = match1Range.length
+
+            formatAction(&mutableContent, match2Range, level)
+
+            let markRange = NSRange(location: match1Range.location, length: match2Range.location - match1Range.location)
+            markEditAction(&mutableContent, markRange, level)
         }
     }
 
